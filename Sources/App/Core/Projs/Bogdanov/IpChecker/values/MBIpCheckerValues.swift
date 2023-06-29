5e70fb5115e8a160dc5618d2eba2a1352c8df03e
Yuri Yunitski <lindio@yandex.ru>
2023-06-29 17:40:09 +0300

    oi

---
+++ Sources/App/Core/Projs/Bogdanov/IpChecker/values/MBIpCheckerValues.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by admin on 20.06.2023.
+//
+
+import Foundation
+
+struct MBIpCheckerStrings {
+    static let strings = """
+            <string name="no_data">No data</string>
+            <string name="country">Country: %1$s</string>
+            <string name="city">City: %1$s</string>
+            <string name="timezone">Timezone: %1$s</string>
+            <string name="query">Query: %1$s</string>
+            <string name="launch">Launch</string>
+            <string name="enter_ip">Enter ip</string>
+            <string name="main">Main</string>
+            <string name="history">History</string>
+        """
+}
