6124b336eda4e69d0a95ddc5a53d61fd4989f2a2
Yuri Yunitski <mvoznicki@gmail.com>
2023-11-08 15:40:10 +0300

    ef meta stage

--- Sources/App/Core/Meta/Description/EmojiFinderMeta.swift
+++ Sources/App/Core/Meta/Description/EmojiFinderMeta.swift
@@ -12,7 +12,7 @@ struct EmojiFinderMeta: MetaProviderProtocol {
     
     static func getFullDesc(appName: String) -> String {
         let fullDesc21 = "\(appName)  is a convenient application for finding emoticons by their name. Just enter a keyword or phrase associated with a smiley face, and the application will provide you with a suitable smiley face, its description and a unique unicode. Thanks to \(appName), you can easily and quickly find the emoticons you need to enrich your communication and express emotions. Supporting a wide range of categories and themes, the app will allow you to easily find emoticons that match any situation - from joy and laughter to sadness and surprise. Regardless of whether you use them in text messages, social networks or email, \(appName) will make the process of finding and adding emoticons fast and convenient. Embody your emotions in bright and expressive emoticons with \(appName)!"
-        let fullDesc = [ fullDesc21]
+        let fullDesc = [fullDesc21]
         
         return fullDesc.randomElement() ?? fullDesc21
     }
